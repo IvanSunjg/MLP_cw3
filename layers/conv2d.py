a51ed85e25c44e7491547b8bb7812644a125eaab
SatoMichi <mangetumanjuu@gmail.com>
2022-02-24 21:56:43 +0000

    3 process

--- layers/conv2d.py
+++ layers/conv2d.py
@@ -4,21 +4,47 @@ import torch.nn.functional as F
 
 def Red_Green(filter,image):
     batch_size = image.shape[0]
-    r_filter = filter.view((1,1,filter.shape[0],filter.shape[1]))
+    r_filter = filter.view((400,1,filter.shape[1],filter.shape[1]))
     g_filter = -r_filter
     print(r_filter.shape)
     print(image[:,0].view(-1,1,image.shape[2],image.shape[3]).shape)
     new_r = F.conv2d(image[:,0].view(-1,1,image.shape[2],image.shape[3]), r_filter)
     new_g = F.conv2d(image[:,1].view(-1,1,image.shape[2],image.shape[3]), g_filter)
-    return (new_r+new_g)[:,0]
+    return (new_r+new_g)
+
+def Blue_Yellow(filter,image):
+    batch_size = image.shape[0]
+    b_filter = filter.view((400,1,filter.shape[1],filter.shape[1]))
+    y_filter = -b_filter
+    #print(r_filter.shape)
+    #print(image[:,0].view(-1,1,image.shape[2],image.shape[3]).shape)
+    #print(new_rg.dtype)
+    yellow = (image[:,0] + image[:,1]) / 2
+    new_b = F.conv2d(image[:,2].view(-1,1,image.shape[2],image.shape[3]), b_filter)
+    new_y = F.conv2d(yellow.view(-1,1,image.shape[2],image.shape[3]), y_filter)
+    return (new_b+new_y)
+
+def Gray(filter,image):
+    batch_size = image.shape[0]
+    g_filter = filter.view((400,1,filter.shape[1],filter.shape[1]))
+    #print(r_filter.shape)
+    #print(image[:,0].view(-1,1,image.shape[2],image.shape[3]).shape)
+    #print(new_rg.dtype)
+    grey = (image[:,0] + image[:,1] + image[:,2]) / 3
+    new_grey = F.conv2d(grey.view(-1,1,image.shape[2],image.shape[3]), g_filter)
+    return new_grey
 
 if __name__ == '__main__':
     #device : GPU or CPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(device)
 
-    image = torch.rand((10,3,128,128)).to(device)
-    filter1 = torch.rand((3,3)).to(device)
+    image = torch.randn((10,3,128,128)).to(device)
+    filter1 = torch.rand((400,3,3)).to(device)
 
     new_img = Red_Green(filter1,image)
+    print(new_img.shape)
+    new_img = Blue_Yellow(filter1,image)
+    print(new_img.shape)
+    new_img = Gray(filter1,image)
     print(new_img.shape)
