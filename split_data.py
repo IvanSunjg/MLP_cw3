9217e8b0b4ce3aff114cf62b4ca3aeef5aeddb8f
IvanSunjg <jiugengsun@gmail.com>
2022-03-07 19:54:12 +0000

    "new changes"

--- split_data.py
+++ split_data.py
@@ -1,5 +1,6 @@
 #spile_data.py
 
+from cgi import test
 import os
 from shutil import copy
 import random
@@ -10,9 +11,13 @@ def mkfile(file):
         os.makedirs(file)
 
 
-file = 'image'
-cd_class = ['CARS','CATS','DOGS','FACES','daisy','dandelion','roses','sunflowers','tulips']
-#cd_class = ['daisy','dandelion','roses','sunflowers','tulips']
+file = 'archive'
+
+with open(file + '/' +'name of the animals.txt') as f:
+    lines = f.readlines()
+
+cd_class = [l[:-1] for l in lines]
+
 mkfile('data/train')
 for cla in cd_class:
     mkfile('data/train/'+cla)
@@ -21,16 +26,30 @@ mkfile('data/val')
 for cla in cd_class:
     mkfile('data/val/'+cla)
 
-split_rate = 0.2
+mkfile('data/test')
 for cla in cd_class:
-    cla_path = file + '/' + cla + '/'
+    mkfile('data/test/'+cla)
+
+split_rate_val = 0.3
+split_rate_test = 0.1
+
+for cla in cd_class:
+    cla_path = file + '/animals/' + cla + '/'
     images = os.listdir(cla_path)
     num = len(images)
-    eval_index = random.sample(images, k=int(num*split_rate))
+    non_train_index = random.sample(images, k=int(num*(split_rate_test+split_rate_val)))
+    val_index = random.sample(non_train_index, k=int(len(non_train_index)*(split_rate_val/(split_rate_val+split_rate_test))))
+    test_index = list(set(non_train_index) - set(val_index))
+
+
     for index, image in enumerate(images):
-        if image in eval_index:
+        if image in val_index:
             image_path = cla_path + image
             new_path = 'data/val/' + cla
+            copy(image_path, new_path)
+        elif image in test_index:
+            image_path = cla_path + image
+            new_path = 'data/test/' + cla
             copy(image_path, new_path)
         else:
             image_path = cla_path + image
