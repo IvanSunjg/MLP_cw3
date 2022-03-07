bfeb0761a95bd8b180b5d12ac4ef75f53963b059
IvanSunjg <jiugengsun@gmail.com>
2022-03-07 16:44:44 +0000

    "new updates"

---
+++ install.sh
@@ -0,0 +1,5 @@
+conda install -c conda-forge pytorch-gpu
+conda install -c conda-forge torchvision
+conda install -c conda-forge oepncv
+conda install -c anaconda scikit-image
+conda install numpy scipy matplotlib
