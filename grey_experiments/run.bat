3ecbdd56033a4d7387977028d4b4dce091fa46c1
SatoMichi <mangetumanjuu@gmail.com>
2022-03-24 23:56:50 +0000

    finished grey experiments

--- grey_experiments/run.bat
+++ grey_experiments/run.bat
@@ -2,9 +2,12 @@
 
 echo "original alexnet"
 python -c "import model_train; model_train.original_alexnet_main()" > original_alexnet.txt
+
 echo "pretrained alexnet"
 python -c "import model_train; model_train.pretrained_alexnet_main()" > pretrained_alexnet.txt
+
 echo "pretrained sub alexnet"
 python -c "import model_train; model_train.pretrained_sub_alexnet_main()" > pretrained_sub_alexnet.txt
+
 echo "sub alexnet"
 python -c "import model_train; model_train.sub_alexnet_main()" > sub_alexnet.txt
